6fa42addbbb96ceef58d09151807d5adca3985b0
Filipe S Oliveira <filipe-santana@outlook.com>
2022-05-26 09:31:51 -0300

    alteracoes

--- resources/views/tarefa/show.blade.php
+++ resources/views/tarefa/show.blade.php
@@ -4,7 +4,7 @@
     <div class="card-header">Tarefa {{ $tarefa->tarefa }}</div>
 
     <div class="card-body">
-        <b>Data de Conclusão:</b> {{ $tarefa->data_limite_conclusao }}
+        <b>Data de Conclusão: </b> {{ $tarefa->data_limite_conclusao }}
         <p><a href='{{ route('tarefa.index') }}' class="btn btn-primary">Voltar</a></p>
     </div>
 @endsection
